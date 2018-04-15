77b57cc2724b168338886c632c1bfb10418577b8
robertcampbell <robcampbell73@gmail.com>
2018-04-15 16:45:33 +1000

    shoppingCartV1 add toggle quantity, watch and ajax call to get items. TODO why quantity not updating straight away with returned items from AJAX call

--- public/script.js
+++ public/script.js
@@ -1,41 +1,89 @@
+var PRICE = 9.99;
 new Vue({
     el: '#app',
     data: {
+        search: '',
         products: [
-            { name: 'apple',
+            {
+                id: 1,
+                name: 'apple',
               price: 19.99,
               quantity: 0
             },
             {
+                id: 2,
                 name: 'orange',
                 price: 12.32,
                 quantity: 0
             },
             {
+                id: 3,
                 name: 'pear',
                 price: 45.01,
                 quantity: 0
             }
         ],
+        filteredProducts: [
+
+        ],
         cart: {
             items: []
+        }
+    },
+    watch: {
+        // whenever search changes, this function will run
+        search: function (newSearch, oldSearch) {
+            //this.filteredProducts = this.filterProducts(newSearch);
+        }
+    },
+    filters: {
+        productId: function(id) {
+            return 'Fruit_sku_'+id;
+        },
+        currency: function(value) {
+            return '$'+value.toFixed(2);
         }
     },
     computed: {
         calculateTotal: function () {
             var currentTotal = 0;
             for (i = 0; i < this.cart.items.length; i++) {
-                currentTotal += (this.cart.items[i].price * this.cart.items[i].quantity);
+                currentTotal += this.calculateSubTotal(this.cart.items[i]);
             }
             return currentTotal;
         },
     },
         methods: {
+            filterProducts: function(search) {
+                var options = {
+                    shouldSort: true,
+                    threshold: 0.6,
+                    location: 0,
+                    distance: 100,
+                    maxPatternLength: 32,
+                    minMatchCharLength: 1,
+                    keys: [
+                        "name",
+                    ]
+                };
+                var fuse = new Fuse(this.products, options);
+                var filteredProducts = fuse.search(search);
+                return filteredProducts;
+            },
+            calculateSubTotal: function (item) {
+                var subTotal = 0;
+
+                subTotal =  item.price * item.quantity;
+
+                return subTotal;
+            },
+
             addItem: function (item) {
                 var itemInCartIndex = this.isItemInCartIndex(item);
 
                 if(itemInCartIndex === false) {
-                    item.quantity++;
+                    item.quantity = 1;
+                    item.price = PRICE;
                     this.cart.items.push(item);
                 } else {
                     this.cart.items[itemInCartIndex].quantity++;
@@ -55,12 +103,25 @@ new Vue({
             isItemInCartIndex: function (item) {
                 var itemInCartIndex = false;
                 for (i = 0; i < this.cart.items.length; i++) {
-                    if (this.cart.items[i].name == item.name) {
+                    if (this.cart.items[i].id == item.id) {
                         itemInCartIndex = i;
-                        continue;
+                        break;
                     }
                 }
                 return itemInCartIndex;
+            },
+
+            onSubmit: function() {
+                //this.filteredProducts = this.filterProducts(this.search);
+
+                // GET /someUrl
+                this.$http.get('/search/'+this.search).then(response => {
+                    // get body data
+                    this.filteredProducts = response.body;
+                }, response => {
+                    // error callback
+                });
+
             }
 
         }
